431af26a449333555e2a580026a4529f4dfe06cf
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-14 04:32:06 +0530

    [Ajay] Welcome to Basic Logical Programs

---
+++ BasicLogicalPrograms/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicLogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Logical Program");
+        }
+    }
+}
